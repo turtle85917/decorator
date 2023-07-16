6511ff16d06a16b98f29ff19f70bd0c6b95b3556
turtle85917 <siu85917@gmail.com>
2023-07-16 18:24:36 +0900

    Study meta data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata";
+
 @ClassDecorator
 class TestClass{
   @MethodDecorator("Hello, there.")
@@ -12,11 +14,15 @@ class TestClass{
   public d(@ParameterDecorator param:string):void{
     console.log(param);
   }
+
+  @Get("/user")
+  public test_():void{
+  }
 }
 
 function ClassDecorator<T extends {new(...args:any[]):{}}>(constructor:T):T{
+  console.log(Reflect.get(constructor.prototype, "path"));
   return class extends constructor{
-    // ...
   };
 }
 
@@ -30,6 +36,12 @@ function MethodDecorator(content:string):(...args: any)=>void{
 
 function ParameterDecorator(target:any, key:string, index:number):void{
   console.log(`${key} : ${index}`);
+}
+
+function Get(path:string):(...args: any)=>void{
+  return (target:any, key:string, descriptor:PropertyDecorator):void=>{
+    Reflect.set(target, "path", path);
+  }
 }
 
 const testClass = new TestClass();
