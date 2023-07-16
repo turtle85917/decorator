8f13fcf34f7f4a89a740ed20f040e2a83633f6ec
turtle85917 <siu85917@gmail.com>
2023-07-16 17:23:17 +0900

    Implement method decorator

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 @ClassDecorator
 class TestClass{
+  @MethodDecorator
+  public deco():void{
+    console.log("Hi.");
+  };
+
   public test():void{
     console.log("Hello, World!");
   }
 }
 
-function ClassDecorator(constructor:typeof TestClass):void{
-  console.log("Evaluated.");
-  constructor.prototype.test();
+function ClassDecorator<T extends {new(...args:any[]):{}}>(constructor:T):T{
+  return class extends constructor{
+    // ...
+  };
+}
+
+function MethodDecorator(target:TestClass, key:string, descriptor:PropertyDescriptor):void{
+  descriptor.value = function(){
+    console.log("Hmm...");
+  };
 }
 
 const testClass = new TestClass();
 testClass.test();
+testClass.deco();
